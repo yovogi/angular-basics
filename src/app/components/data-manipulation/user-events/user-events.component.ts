a105508085a955dc7846e483dcfc37de3c547392
Yovo Gitsov <ext-yovo.gitsov@p3ds.net>
2022-07-26 14:33:34 +0300

    Task - User events

--- src/app/components/data-manipulation/user-events/user-events.component.ts
+++ src/app/components/data-manipulation/user-events/user-events.component.ts
@@ -3,9 +3,28 @@ import { Component } from "@angular/core";
 @Component({
     selector: "user-events",
     templateUrl: "./user-events.component.html",
-    styleUrls: ["./user-events.component.scss"]
+    styleUrls: ["./user-events.component.scss"],
 })
 export class UserEventsComponent {
     public lastPressedKey: string = "Implement This";
     public isRedSquareHovered: boolean = false;
+    isHovered: boolean = false;
+
+    onKeyUp(event) {
+        this.lastPressedKey = event.key;
+    }
+
+    onAlert() {
+        alert("something wrong!");
+    }
+
+    turnOnHover() {
+        this.isHovered = true;
+        console.log("Turning on hovering");
+    }
+
+    turnOffHover() {
+        this.isHovered = false;
+        console.log("Turning off hovering");
+    }
 }
